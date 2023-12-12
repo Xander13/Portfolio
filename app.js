04ba179f53baa0279f4a7efe9ec7a534d7791781
Alex Kauffman <xanderkau13@gmail.com>
2023-12-12 02:12:09 +0000

    v12

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.top = "24px";
   } else {
     document.getElementById("navbar").style.top = "-96px";
   }
   prevScrollpos = currentScrollPos;
+}
+
+// Get the button:
+var mybutton = $("#toTop");
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
