9ac3ff3b7337c268dbbfdf087287bc02ded88646
Alex Kauffman <xanderkau13@gmail.com>
2024-03-28 03:45:08 +0000

    v12

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "24px";
-  } else {
-    document.getElementById("navbar").style.top = "-96px";
-  }
-  prevScrollpos = currentScrollPos;
-}
-
 //copy text to clipboard
 let text = 'xanderkau13@gmail.com';
+
+const changeText = document.querySelector("#change-text");
+
+// Initial message
+changeText.textContent = "Click to copy email to clipboard";
+
 const copyContent = async () => {
   try {
     await navigator.clipboard.writeText(text);
     console.log('Content copied to clipboard');
+    changeText.textContent = "Copied!";
+    // Timeout to change the message
+    setTimeout(() => {
+      changeText.textContent = "Click to copy email to clipboard";
+    }, 2000);
   } catch (err) {
     console.error('Failed to copy: ', err);
+    changeText.textContent = "Failed to copy. Please try manually.";
   }
-}
+};
+
+changeText.addEventListener("click", copyContent);
 
 // Get the button:
 var mybutton = $("#toTop");
@@ -28,3 +30,9 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+//toogle light and dark mode
+const toggleButton = document.getElementById('toggle-button');
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('lightMode');
+});
