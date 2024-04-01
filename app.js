3daa922cae94bf5770ad6e74a708c11197f9f289
Alex Kauffman <xanderkau13@gmail.com>
2024-04-01 01:30:18 +0000

    v12

--- app.js
+++ app.js
@@ -23,16 +23,69 @@ const copyContent = async () => {
 
 changeText.addEventListener("click", copyContent);
 
-// Get the button:
-var mybutton = $("#toTop");
 
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+//Image flipper 
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll("#video-container .image");
+  let index = 0;
+
+  function showImage() {
+    images.forEach(image => {
+      image.classList.remove("active");
+    });
+    images[index].classList.add("active");
+    index = (index + 1) % images.length;
+    setTimeout(showImage, 1200);
+  }
+  showImage();
+});
 
 //toogle light and dark mode
 const toggleButton = document.getElementById('toggle-button');
 toggleButton.addEventListener('click', () => {
   document.body.classList.toggle('lightMode');
+});
+
+// Function to reset the scroll position to the top
+function resetScroll() {
+  window.scrollTo(0, 0);
+}
+
+// Event listener for scrolling
+window.addEventListener('scroll', function () {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    resetScroll();
+  }
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  //motion text
+  const textLines = document.querySelectorAll('.line');
+  let delay = 0;
+
+  textLines.forEach(function (line) {
+    line.style.animationDelay = delay + 's';
+    delay += 0.5; // Increase delay for each line
+  });
+
+  //cursor
+  const cursorRounded = document.querySelector('#arrow');
+
+  const moveCursor = (e) => {
+    const mouseY = e.clientY;
+    const mouseX = e.clientX;
+    cursorRounded.style.transform = `translate3d(${mouseX - 160}px, ${mouseY - 160}px, 0)`;
+  }
+
+  window.addEventListener('mousemove', moveCursor);
+
+  //Clickable drop down
+  const drawers = document.querySelectorAll('.drawer');
+
+  drawers.forEach(drawer => {
+    drawer.addEventListener('click', function () {
+      this.classList.toggle('opened');
+    });
+  });
 });
