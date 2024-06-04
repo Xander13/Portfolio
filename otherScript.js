ec6b18bb67dbcdb279707d4aa3aab23d0675543d
Alex Kauffman <xanderkau13@gmail.com>
2024-06-04 04:53:54 +0000

    v12

--- otherScript.js
+++ otherScript.js
@@ -15,42 +15,4 @@ window.addEventListener('scroll', checkScroll);
 
 // Event listener for window resize
 window.addEventListener('resize', checkScroll);
-
-
-//toogle light and dark mode
-const toggleButton = document.getElementById('toggle-button');
-toggleButton.addEventListener('click', () => {
-  document.body.classList.toggle('lightMode');
-});
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  //motion text
-  const textLines = document.querySelectorAll('.line');
-  let delay = 0;
-
-  textLines.forEach(function (line) {
-    line.style.animationDelay = delay + 's';
-    delay += 0.5; // Increase delay for each line
-  });
-
-  //cursor
-  const cursorRounded = document.querySelector('#arrow');
-
-  const moveCursor = (e) => {
-    const mouseY = e.clientY;
-    const mouseX = e.clientX;
-    cursorRounded.style.transform = `translate3d(${mouseX - 160}px, ${mouseY - 160}px, 0)`;
-  }
-
-  window.addEventListener('mousemove', moveCursor);
-
-  //Clickable drop down
-  const drawers = document.querySelectorAll('.drawer');
-
-  drawers.forEach(drawer => {
-    drawer.addEventListener('click', function () {
-      this.classList.toggle('opened');
-    });
-  });
-});
+  
