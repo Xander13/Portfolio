e945d0980d4d42a00c9771661cace421f4c4d654
Alex Kauffman <xanderkau13@gmail.com>
2026-08-25 05:08:31 +0000

    v12

--- js/essence.js
+++ js/essence.js
@@ -1697,7 +1697,7 @@ const personalSynonyms = {
     resume: ["resume", "alex's resume", "CV", "cv"],
     coding: ["coding", "code", "programming", "development", "engineering", "cs", "computer science", "algorithm", "algorithms", "sort", "sorting"],
     Favcolor: ["What is Alex's favorite color", "fav color", "favorite Color"],
-    inventions: ["What Alex's favorite inventions", "fav inventions", "favorite inventions"]
+    Favproduct: ["What Alex's favorite product", "fav product", "favorite product"]
 };
 
 
@@ -1853,9 +1853,9 @@ Impact: Proved how platforms can tap into major underserved markets without comp
         };
     }
 
-    if (matchedKeys.includes("inventions")) {
+    if (matchedKeys.includes("Favproduct")) {
         return {
-            text: knowledge.personal["inventions"],
+            text: knowledge.personal["Favproduct"],
             instant: true
         };
     }
